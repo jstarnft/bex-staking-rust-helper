504987e1d9f15e0e5045907b1ae8b570a0298cdd
wyf-ACCEPT <define_wifi@163.com>
2023-12-09 21:32:31 +0800

    cp: try abi encoding

--- src/signature.rs
+++ src/signature.rs
@@ -1,4 +1,13 @@
+use hex;
+use std::time::{SystemTime, UNIX_EPOCH};
 use ethers::abi::{encode_packed, Token};
+
+pub fn now_timestamp() -> u64 {
+    let current_time = SystemTime::now();
+    let since_epoch = current_time.duration_since(UNIX_EPOCH).unwrap();
+    let seconds = since_epoch.as_secs();
+    seconds
+}
 
 pub fn try_abi_encode() {
     /*
@@ -9,12 +18,19 @@ pub fn try_abi_encode() {
      * b54e978a34af50228a3564662db6005e9fb04f5a
      * 0000000000000000000000000000000000000000000000000000000065705d53
      */
-    let encoded = encode_packed(&[
-        Token::Bytes("cb62320d".into()), 
-        Token::String("hi".into()),
-        Token::Uint("7000000000".parse().unwrap()),
+
+    let selector = "cb62320d";
+    let binder_name = "hi";
+    let amount = 70000_000000_000000_u128;
+    let amount_padded_hex = format!("{:0>64}", format!("{:016X}", amount));
+    let timestamp_padded_hex = format!("{:0>64}", format!("{:016X}", now_timestamp()));
+
+    let packed_data: Vec<u8> = encode_packed(&[
+        Token::Bytes(hex::decode(selector).unwrap().into()), 
+        Token::String(binder_name.into()),
+        Token::Bytes(hex::decode(amount_padded_hex).unwrap().into()),
         Token::Address("b54e978a34af50228a3564662db6005e9fb04f5a".parse().unwrap()),
-        Token::Uint("1701862739".parse().unwrap()),
+        Token::Bytes(hex::decode(timestamp_padded_hex).unwrap().into())
     ]).unwrap();
-    println!("{:?}", encoded);
+    println!("{:?}", hex::encode(&packed_data));
 }
