cc2af6f30797f8576769cc6316570a66d0c0245e
wyf-ACCEPT <define_wifi@163.com>
2023-12-10 14:37:23 +0800

    feat: finish `sign_base_requset`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,43 @@
 pub mod signature;
 
+use std::env;
+use dotenv::dotenv;
+
+use ethers::signers::LocalWallet;
+
+async fn async_main() -> Result<(), Box<dyn std::error::Error>> {
+    // Load wallet from .env
+    dotenv().ok();
+    let wallet;
+    if let Ok(private_key) = env::var("PRIVATE_KEY_ADMIN") {
+        wallet = private_key.parse::<LocalWallet>()?;
+        println!("{:?}", wallet);
+    } else {
+        panic!("Variable `PRIVATE_KEY_ADMIN` not in `.env`!");
+    }
+
+    // Sign base request
+    let (time, message, sig) = signature::sign_base_requset("cb62320d", "hi", 70000000000000000, "0xb54e978a34Af50228a3564662dB6005E9fB04f5a", signature::now_timestamp(), wallet).await;
+
+    println!("time: {}", time);
+    println!("message: {}", message);
+    println!("sig: {}", sig);
+
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
-    signature::try_abi_encode();
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(2)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(async {
+        match async_main().await {
+            Ok(_) => println!("Async operation completed successfully"),
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    });
 }
--- src/signature.rs
+++ src/signature.rs
@@ -1,6 +1,8 @@
 use hex;
 use std::time::{SystemTime, UNIX_EPOCH};
 use ethers::abi::{encode_packed, Token};
+use ethers::types::Signature;
+use ethers::signers::Signer;
 
 pub fn now_timestamp() -> u64 {
     let current_time = SystemTime::now();
@@ -9,28 +11,61 @@ pub fn now_timestamp() -> u64 {
     seconds
 }
 
-pub fn try_abi_encode() {
-    /*
-     * 0x
-     * cb62320d
-     * 6869
-     * 00000000000000000000000000000000000000000000000000f8b0a10e470000
-     * b54e978a34af50228a3564662db6005e9fb04f5a
-     * 0000000000000000000000000000000000000000000000000000000065705d53
-     */
-
-    let selector = "cb62320d";
-    let binder_name = "hi";
-    let amount = 70000_000000_000000_u128;
+/// Signs a base request with specified parameters and a signer.
+/// 
+/// This function prepares a message for signing by encoding various parameters into a hex string.
+/// It then uses the provided signer to generate a signature for the message.
+///
+/// # Arguments
+/// * `selector` - A hexadecimal string representing the function selector.
+/// * `name` - A string parameter, typically representing a name.
+/// * `amount` - A u128 integer representing an amount, encoded into a 64-character zero-padded hex string.
+/// * `user` - A string representing the user's address.
+/// * `time` - A u64 timestamp, encoded into a 64-character zero-padded hex string.
+/// * `signer` - An implementation of the `Signer` trait, used to sign the message.
+///
+/// # Returns
+/// A tuple containing:
+/// * The original timestamp (`u64`).
+/// * A string representation of the message in hex format.
+/// * The generated `Signature`.
+///
+/// # Examples
+/// ```
+/// // Example usage
+/// let (timestamp, message, signature) = sign_base_request(
+///     "cb62320d",
+///     "Alice",
+///     1000000,
+///     "b54e978a34af50228a3564662db6005e9fb04f5a",
+///     1651015203,
+///     signer_instance
+/// );
+/// ```
+///
+/// # Errors
+/// This function will return an error if the hex decoding fails or if the signer fails to sign the message.
+pub async fn sign_base_requset(
+    selector: &str,
+    name: &str,
+    amount: u128,
+    user: &str,
+    time: u64,
+    signer: impl Signer,
+) -> (u64, String, Signature) {
     let amount_padded_hex = format!("{:0>64}", format!("{:016X}", amount));
-    let timestamp_padded_hex = format!("{:0>64}", format!("{:016X}", now_timestamp()));
+    let timestamp_padded_hex = format!("{:0>64}", format!("{:016X}", time));
 
-    let packed_data: Vec<u8> = encode_packed(&[
+    let message_hexstring = encode_packed(&[
         Token::Bytes(hex::decode(selector).unwrap().into()), 
-        Token::String(binder_name.into()),
+        Token::String(name.into()),
         Token::Bytes(hex::decode(amount_padded_hex).unwrap().into()),
-        Token::Address("b54e978a34af50228a3564662db6005e9fb04f5a".parse().unwrap()),
+        Token::Address(user.parse().unwrap()),
         Token::Bytes(hex::decode(timestamp_padded_hex).unwrap().into())
     ]).unwrap();
-    println!("{:?}", hex::encode(&packed_data));
+    let message_bytes = hex::encode(&message_hexstring);
+
+    let signature = Signer::sign_message(&signer, &message_hexstring)
+        .await.unwrap();
+    (time, message_bytes, signature)
 }
